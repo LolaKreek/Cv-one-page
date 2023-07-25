18ba3820a039ab98704abea66262bac2b5497d8b
Lalita Klimchuk <lalitaklimchuk@gmail.com>
2023-07-25 14:16:25 +0300

    Added carousel for main photos

--- src/components/AboutMe/index.tsx
+++ src/components/AboutMe/index.tsx
@@ -1,15 +1,41 @@
 import "./style.css";
-import mainImage from "../../assets/mainImage.jpg"
+import MainFirstImage from "../../assets/main-first-image.jpg";
+import MainSecondImage from "../../assets/main-second-image.jpg";
+import MainThierdImage from "../../assets/main-third-image.jpeg";
 import GradientButton from "../GradientButtonWithIcon";
 import EngineeringIcon from '@mui/icons-material/Engineering';
 import ArrowCircleDownIcon from '@mui/icons-material/ArrowCircleDown';
 import EmergingPart from "../EmergingPart";
+import { Tooltip } from "@mui/material";
+import ChevronRightIcon from '@mui/icons-material/ChevronRight';
+import ChevronLeftIcon from '@mui/icons-material/ChevronLeft';
+import { useEffect, useState } from "react";
 
 const AboutMe = () => {
+    const imageArray = [MainFirstImage, MainSecondImage, MainThierdImage];
+    const [imageSetter, setImageSetter] = useState(imageArray[0]);
+    const [currentImage, setCurrentImage] = useState(0);
+
+    useEffect(() => {
+        if(currentImage > (imageArray.length - 1)){
+            setCurrentImage(0);
+        }
+        else if(currentImage < 0){
+            setCurrentImage(imageArray.length - 1);
+        }
+        setImageSetter(imageArray[currentImage]);
+    }, [currentImage])
+
     return(
         <div className="about-me__main-container">
             <div className="about-me-image__main-container">
-                <img className="about-me-image__image-item" src={mainImage} alt="Main image" />
+                <Tooltip className="about-me__arrows-cantainer" title="Go left" onClick={() => setCurrentImage(currentImage - 1)}>
+                    <ChevronLeftIcon sx={{color: "#dbb594", fontSize: 40}}/>
+                </Tooltip>
+                {imageSetter ? <img className="about-me-image__image-item" src={imageSetter} alt="Main image" /> : <></>}
+                <Tooltip className="about-me__arrows-cantainer" title="Go right"  onClick={() => setCurrentImage(currentImage + 1)}>
+                    <ChevronRightIcon sx={{color: "#dbb594", fontSize: 40}} />
+                </Tooltip>
             </div>
 
             <EmergingPart />
