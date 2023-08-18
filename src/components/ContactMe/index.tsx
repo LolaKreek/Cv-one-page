2b72620ee8356a0129982f701c2e9107d331963f
Lalita Klimchuk <lalitaklimchuk@gmail.com>
2023-08-18 17:25:19 +0300

    Added scss styles for contact me part

--- src/components/ContactMe/index.tsx
+++ src/components/ContactMe/index.tsx
@@ -1,13 +1,12 @@
 import ContactIcon from "../../assets/contact_icon.png";
 import ContactMainImg from '../../assets/contact_me.jpg';
 import SendIcon from '@mui/icons-material/Send';
-import "./style.css"
 import { TextField, Tooltip } from "@mui/material";
 import Circles from "../Circles";
 import Circle_01 from "../../assets/circle_03.png";
 import { useFormik } from "formik";
 import { contactMeScheme } from "../../features/formik/formikScheme";
-import { styleContactMeInput, styleContactMeLabels, styleContactMeTextarea, styleContactMeTextareaLabels } from "../stylesVariable";
+import { allInputs, textAreas } from "../stylesVariable";
 
 const ContactMe = () => {
     const formik = useFormik({
@@ -28,43 +27,27 @@ const ContactMe = () => {
 
     return(
         <div id="contactMe" className="main-wrapper__contact-me contact-me">
-            <div className="general">
+            <div>
                 <div className="contact-me__relative-wrapper">
-                    <div className="contact-me__content-wrapper">
-                        <h2 className="contact-me__main-header margin-zero main-header">//Contact me</h2>
-                        <h2 className="contact-me__second-header margin-zero main-header">Let's connect!</h2>
-                        <p className="standard-color-and-font">I am thrilled to be available for contact and excitedly looking forward 
+                    <div className="contact-me__content-wrapper contact-wrapper">
+                        <h2 className="contact-wrapper__main-header main-header">//Contact me</h2>
+                        <h2 className="contact-wrapper__second-header second-header">Let's connect!</h2>
+                        <p className="standard-color-and-font contact-wrapper__paragraph">I am thrilled to be available for contact and excitedly looking forward 
                         to connecting with you! Whether it's regarding career prospects, collaboration opportunities, or simply sharing 
                         ideas, don't hesitate to reach out using the information provided below. As a dedicated and enthusiastic individual, 
                         I'm eager to explore new possibilities and engage in meaningful conversations that can lead to mutual growth and success. 
                         So, let's connect and embark on a journey of innovation and excellence together!</p>
 
-                        <form className='contact-me__form' onSubmit={formik.handleSubmit}>
+                        <form className='contact-wrapper__form' onSubmit={formik.handleSubmit}>
                             <TextField
                                 name="email"
                                 label="Your email"
                                 variant="outlined"
                                 autoComplete="on"
                                 type="text"
-                                className="contact-me__input"
+                                className="form__input"
                                 value={formik.values.email}
-                                sx={{input: styleContactMeInput, label: styleContactMeLabels, margin: "0 0 2% 0", 
-                                    "& .MuiOutlinedInput-root": {
-                                        "& > fieldset": { borderColor: "white", borderRadius: '8px' },
-                                    },
-                                    "& .MuiOutlinedInput-root.Mui-focused": {
-                                        "& > fieldset": { borderColor: "#8a7474" }
-                                    },
-                                    "& .MuiOutlinedInput-root:hover": {
-                                        "& > fieldset": { borderColor: "#aaa" }
-                                    },
-                                    "& .MuiInputLabel-root.Mui-focused": {
-                                        color: '#8a7474'
-                                    },
-                                    "& .MuiFormHelperText-root": {
-                                        textAlign: 'center'
-                                    }
-                                }}
+                                sx={allInputs}
                                 onChange={formik.handleChange}
                                 error={formik.touched.email && Boolean(formik.errors.email)}
                                 helperText={formik.touched.email && formik.errors.email}/>
@@ -75,25 +58,9 @@ const ContactMe = () => {
                                 variant="outlined"
                                 autoComplete="on"
                                 type="text"
-                                className="contact-me__input"
+                                className="form__input"
                                 value={formik.values.name}
-                                sx={{input: styleContactMeInput, label: styleContactMeLabels, margin: "0 0 2% 0",
-                                    "& .MuiOutlinedInput-root": {
-                                        "& > fieldset": { borderColor: "white", borderRadius: '8px' },
-                                    },
-                                    "& .MuiOutlinedInput-root.Mui-focused": {
-                                        "& > fieldset": { borderColor: "#8a7474" }
-                                    },
-                                    "& .MuiOutlinedInput-root:hover": {
-                                        "& > fieldset": { borderColor: "#aaa" }
-                                    },
-                                    "& .MuiInputLabel-root.Mui-focused": {
-                                        color: '#8a7474'
-                                    },
-                                    "& .MuiFormHelperText-root": {
-                                        textAlign: 'center'
-                                    }
-                                }}
+                                sx={allInputs}
                                 onChange={formik.handleChange}
                                 error={formik.touched.name && Boolean(formik.errors.name)}
                                 helperText={formik.touched.name && formik.errors.name} />
@@ -106,32 +73,16 @@ const ContactMe = () => {
                                 type="textarea"
                                 multiline
                                 rows={4}
-                                className="contact-me__input"
+                                className="form__input"
                                 value={formik.values.message}
-                                sx={{textarea: styleContactMeTextarea, label: styleContactMeTextareaLabels, margin: "0 0 3% 0",
-                                    "& .MuiOutlinedInput-root": {
-                                        "& > fieldset": { borderColor: "white", borderRadius: '8px' },
-                                    },
-                                    "& .MuiOutlinedInput-root.Mui-focused": {
-                                        "& > fieldset": { borderColor: "#8a7474" }
-                                    },
-                                    "& .MuiOutlinedInput-root:hover": {
-                                        "& > fieldset": { borderColor: "#aaa" }
-                                    },
-                                    "& .MuiInputLabel-root.Mui-focused": {
-                                        color: '#8a7474'
-                                    },
-                                    "& .MuiFormHelperText-root": {
-                                        textAlign: 'center'
-                                    }
-                                }}
+                                sx={textAreas}
                                 onChange={formik.handleChange}
                                 error={formik.touched.message && Boolean(formik.errors.message)}
                                 helperText={formik.touched.message && formik.errors.message} />
 
-                            <div className="contac-me__submit-wrapper">
-                                <div className="contact-me__submit-container">
-                                    <input className="contact-me__submit" type="submit" value="Send" />
+                            <div className="form__submit-wrapper">
+                                <div className="submit__container">
+                                    <input className="submit__submit-button" type="submit" value="Send" />
                                     <Tooltip title="Sent the message">
                                         <SendIcon />
                                     </Tooltip>
@@ -141,30 +92,30 @@ const ContactMe = () => {
                     </div>
 
                     <div className="contact-me__middle-part">
-                        <img className="contact-me__contact-icon" src={ContactIcon} alt="Contact icon" />
+                        <img className="contact-middle-part__contact-icon" src={ContactIcon} alt="Contact icon" />
                         
-                        <div className="contact-me__middle-part-container">
-                            <p className="contact-me__middle-part-inscription margin-zero standard-color-and-font">lalitaklimchuk@gmail.com</p>
-                            <p className="contact-me__middle-part-inscription margin-zero standard-color-and-font">+48 123 345 567</p>
+                        <div>
+                            <p className="standard-color-and-font">lalitaklimchuk@gmail.com</p>
+                            <p className="standard-color-and-font">+48 123 345 567</p>
                         </div>
                     </div>
 
                     <div className="contact-me__image-wrapper">
-                        <div className="contact-me__main-image-container">
-                            <img className="contact-me__main-image" src={ContactMainImg} alt="Main image in the section contact me" />
+                        <div className="contact-image__image-container">
+                            <img className="contact-image__image" src={ContactMainImg} alt="Main image in the section contact me" />
                         </div>
 
-                        <div className="contact-me__text-container">
-                            <p className="contact-me__description margin-zero standard-color-and-font">author: Lalita Klimchuk</p>
-                            <p className="contact-me__description margin-zero standard-color-and-font">username: @LolaKreek</p>
-                            <p className="contact-me__description margin-zero standard-color-and-font">description: Student</p>
-                            <p className="contact-me__description margin-zero standard-color-and-font">occupation: Front-End Developer</p>
-                            <p className="contact-me__description margin-zero standard-color-and-font">repository type: Open-source</p>
-                            <p className="contact-me__description margin-zero standard-color-and-font">gitHub: https://github.com/LolaKreek</p>
+                        <div className="contact-image__text-container">
+                            <p className="standard-color-and-font">author: Lalita Klimchuk</p>
+                            <p className="standard-color-and-font">username: @LolaKreek</p>
+                            <p className="standard-color-and-font">description: Student</p>
+                            <p className="standard-color-and-font">occupation: Front-End Developer</p>
+                            <p className="standard-color-and-font">repository type: Open-source</p>
+                            <p className="standard-color-and-font">gitHub: https://github.com/LolaKreek</p>
                         </div>
                     </div> 
 
-                    <Circles class="contact-me__container-circle-1" classImg="contact-me__circle-1 circle" src={Circle_01} />
+                    <Circles class="contact-me__container-circle-1 circle" classImg="circle-1" src={Circle_01} />
                 </div>
             </div>
         </div>
--- src/components/stylesVariable.tsx
+++ src/components/stylesVariable.tsx
@@ -1,4 +1,4 @@
-export const styleContactMeInput = {
+const styleContactMeInput = {
     color: '#8a7474',
     fontWeight: 'normal',
     fontSize: '20px',
@@ -6,23 +6,61 @@ export const styleContactMeInput = {
     margin: '10px 0 15px 0'
 }
 
-export const styleContactMeTextarea = {
+const styleContactMeTextarea = {
     color: '#8a7474',
     fontWeight: 'normal',
     fontSize: '20px',
     margin: '0 0 0 5px'
 }
 
-export const styleContactMeLabels = {
+const styleContactMeLabels = {
     color: '#aaa',
     fontWeight: 'normal',
     fontSize: "18px",
     margin: '0 0 0 -3px'
 }
 
-export const styleContactMeTextareaLabels = {
+const styleContactMeTextareaLabels = {
     color: '#aaa',
     fontWeight: 'normal',
     fontSize: "18px",
     margin: '0 0 0 -2px'
+}
+
+export const allInputs = {
+    input: styleContactMeInput, label: styleContactMeLabels, margin: "0 0 2% 0", 
+    "& .MuiOutlinedInput-root": {
+        "& > fieldset": { borderColor: "white", borderRadius: '8px', transition: 'all .5s' },
+    },
+    "& .MuiOutlinedInput-root.Mui-focused": {
+        "& > fieldset": { borderColor: "#8a7474" }
+    },
+    "& .MuiOutlinedInput-root:hover": {
+        "& > fieldset": { borderColor: "#aaa", transition: 'all .5s' }
+    },
+    "& .MuiInputLabel-root.Mui-focused": {
+        color: '#8a7474'
+    },
+    "& .MuiFormHelperText-root": {
+        textAlign: 'center'
+    }
+}
+
+export const textAreas ={
+    textarea: styleContactMeTextarea, label: styleContactMeTextareaLabels, margin: "0 0 3% 0",
+    "& .MuiOutlinedInput-root": {
+        "& > fieldset": { borderColor: "white", borderRadius: '8px', transition: 'all .5s' },
+    },
+    "& .MuiOutlinedInput-root.Mui-focused": {
+        "& > fieldset": { borderColor: "#8a7474" }
+    },
+    "& .MuiOutlinedInput-root:hover": {
+        "& > fieldset": { borderColor: "#aaa", transition: "all .5s" }
+    },
+    "& .MuiInputLabel-root.Mui-focused": {
+        color: '#8a7474'
+    },
+    "& .MuiFormHelperText-root": {
+        textAlign: 'center'
+    }
 }
