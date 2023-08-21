fd638b6220abf4a31d11d68aa8088235353150c3
Lalita Klimchuk <lalitaklimchuk@gmail.com>
2023-08-21 04:48:02 +0200

    Updated tools styles

--- src/components/ToolItem/index.tsx
+++ src/components/ToolItem/index.tsx
@@ -3,7 +3,7 @@ import { ComponentsTypes } from "../types";
 const ToolItem = (props: ComponentsTypes.ToolsItemPropsType) => {
     return(
         <div className={'tools__container-icons ' + props.wrapper}>
-            <a href={props.href} target="blank">
+            <a className="tools__icons-link" href={props.href} target="blank">
                 <img alt='Tools icon' className='tools__icons' src={props.src} />
                 <p className='tools__inscription'>{props.firstText}</p>
                 <p className='tools__inscription'>{props.secondText}</p>
