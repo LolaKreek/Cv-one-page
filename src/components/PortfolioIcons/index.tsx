f9c91433dc3a3a4ff8ad85d199fafa14110fedb2
Lalita Klimchuk <lalitaklimchuk@gmail.com>
2023-08-21 03:17:51 +0200

    Added scss styles for portfolio icons

--- src/components/PortfolioIcons/index.tsx
+++ src/components/PortfolioIcons/index.tsx
@@ -3,25 +3,24 @@ import GitHubIcon from '@mui/icons-material/GitHub';
 import WebIcon from '@mui/icons-material/Web';
 import { Tooltip } from '@mui/material';
 import { ComponentsTypes } from '../types';
-import "./style.css";
 
 const PortfolioIcons = (props: ComponentsTypes.PortfolioIconsType) => {
     return (
-        <div className="my-works__container-icons">
+        <div className="portfolio__container-icons">
             <Tooltip title="GitHub">
-                <a className='my-works__icon-item' href="https://github.com/LolaKreek" target='_blank'>
+                <a className='portfolio__icon-item' href="https://github.com/LolaKreek" target='_blank'>
                     <GitHubIcon sx={{fontSize: 32, color: "#c8c8c7"}} />
                 </a>
             </Tooltip>
 
             <Tooltip title="Code of this project">
-                <a className='my-works__icon-item' href={props.href} target='_blank'>
+                <a className='portfolio__icon-item' href={props.href} target='_blank'>
                     <LogoDevIcon sx={{fontSize: 35, color: "#c8c8c7"}} />
                 </a>
             </Tooltip>
 
             {props.showWebsite ? <Tooltip title="Open this project">
-                <a className='my-works__icon-item' href={props.showWebsite} target='_blank'>
+                <a className='portfolio__icon-item' href={props.showWebsite} target='_blank'>
                     <WebIcon sx={{fontSize: 39, color: "#c8c8c7"}} />
                 </a>
             </Tooltip> : <></>}
