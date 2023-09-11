0fe8cb09a485c1f6df0fcabd364f7bc4f407ff9e
Lalita Klimchuk <lalitaklimchuk@gmail.com>
2023-09-11 20:25:24 +0300

    Added translation into two languages of the blogs part

--- src/components/BlogItem/index.tsx
+++ src/components/BlogItem/index.tsx
@@ -1,6 +1,7 @@
 import ArrowForwardIcon from '@mui/icons-material/ArrowForward';
 import { Tooltip } from "@mui/material";
 import { ComponentsTypes } from "../types";
+import { useTranslation } from 'react-i18next';
 
 //Creating one blog post in the "Blogs" part
 //PROPS:
@@ -8,26 +9,25 @@ import { ComponentsTypes } from "../types";
 //src           -> link to picture
 //title         -> title text (main title in this component, but second title in "Blogs" part) 
 const BlogItem = (props: ComponentsTypes.BlogItemPropsType) => {
+    const {t} = useTranslation();
     return(
         <div className={"blog-posts__container-posts " + props.wrapper}>
             <img className='blog-posts__main-image' alt='Blogs' src={props.src} />
                     
             <div className="post-content">
                 <h2 className="post-content__main-post-header main-header">{props.title}</h2>
-                <h2 className="post-content__second-post-header second-header">Secondary Title</h2>
-                <p className='post-content__paragraph standard-color-and-font'>Ut enim ad minim veniam, quis nostrud exercitation 
-                    ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate 
-                    velit esse cillum dolore eu fugiat nulla pariatur.</p>
+                <h2 className="post-content__second-post-header second-header">{t('blogItemHeader')}</h2>
+                <p className='post-content__paragraph standard-color-and-font'>{t('blogSecondDescription')}</p>
 
                 <div className="post-content__content-read-more">
-                    <p className="content__read-more">Read more</p>
+                    <p className="content__read-more">{t('blogReadMore')}</p>
                     <Tooltip title="Read more" className="content__read-more-arrow">
                         <ArrowForwardIcon />
                     </Tooltip>
                 </div>
 
                 <div className="post-content__author-container">
-                    <p className="author-container__author">author, 01.09.2020</p>
+                    <p className="author-container__author">{t('blogAuthor')}, 01.09.2020</p>
                 </div>
             </div>
         </div>
--- src/components/BlogPosts/index.tsx
+++ src/components/BlogPosts/index.tsx
@@ -2,14 +2,17 @@ import BlogItem from "../BlogItem";
 import Circles from "../Circles";
 import Circle_01 from "../../assets/circle_01.png";
 import Circle_02 from "../../assets/circle_02.png";
+import { useTranslation } from "react-i18next";
 
 //Creating a part "Blogs"
 const BlogPosts = () => {
+    const {t} = useTranslation();
+
     return(
         <div id="blog" className="blog-posts">
             <div className="general">
-                <h2 className='main-header'>//Blog posts</h2>
-                <h2 className='second-header'>Hints and tips</h2>
+                <h2 className='main-header'>{t('blogPosts')}</h2>
+                <h2 className='second-header'>{t('blogSecondHeader')}</h2>
 
                 <Circles class="blog-posts__container-circle-1 circle" classImg="circle-1" src={Circle_01} />
                 <Circles class="blog-posts__container-circle-2 circle" classImg="circle-2" src={Circle_02} />
@@ -17,11 +20,11 @@ const BlogPosts = () => {
                 <div className="blog-posts__content-main-container">
                     <BlogItem wrapper='post-1' 
                         src='https://lenovogaming.pl/media/cms_pages_category/image/12/2206/01_Legion_Tower_5_Full_Gaming_Battle_Station_wasko.jpg?ts=1602246057'
-                        title='//Title 01' />
+                        title={t('blogItemTitle')} />
 
                     <BlogItem wrapper='post-2' 
                         src='https://cdn.pkt.pl/f-13195-komputery-stacjonarne-dedykowane-dla-graczy.jpg'
-                        title='//Title 02' />
+                        title={t('blogItemTitle')} />
                 </div>
             </div>
         </div>
