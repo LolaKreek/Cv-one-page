11cc9e0a351ec8d3de514caa765ab20a45e6a5de
Lalita Klimchuk <lalitaklimchuk@gmail.com>
2023-09-11 15:57:24 +0300

    Added translation into two languages of the works part

--- src/components/MyWorks/index.tsx
+++ src/components/MyWorks/index.tsx
@@ -6,25 +6,21 @@ import UnderConstruction from '../../assets/under-construction.png';
 import PortfolioIcons from "../PortfolioIcons";
 import Circles from "../Circles";
 import Circle_01 from "../../assets/circle_01.png"
+import { useTranslation } from "react-i18next";
 
 //Creating a part "My works/Portfolio"
 const MyWorks = () => {
     const [firstWork, setFirstWork] = useState(false);
     const [secondWork, setSecondWork] = useState(false);
     const [thirdWork, setThirdWork] = useState(false);
+    const {t} = useTranslation();
 
     return(
         <div className="my-works">
-            <h2 className="main-header">//My works</h2>
-            <h2 className="second-header">Portfolio</h2>
+            <h2 className="main-header">{t('works')}</h2>
+            <h2 className="second-header">{t('worksPortfolio')}</h2>
 
-            <p className="standard-color-and-font my-works__text">Welcome to my evolving portfolio of programming projects, where quality 
-            reigns supreme over quantity. While my collection may not be extensive, rest assured that each project is imbued 
-            with soul, passion, and unwavering commitment. With every line of code written, I embark on a journey of continuous 
-            improvement, determined to hone my skills and create software solutions that leave a lasting impression. As the years 
-            pass, my portfolio will undoubtedly grow in both scope and sophistication, becoming a true reflection of my dedication 
-            to the craft and my relentless pursuit of excellence. So, join me on this dynamic journey, where every project tells 
-            a unique story of growth, learning, and the unwavering desire to make a difference through the power of programming.</p>
+            <p className="standard-color-and-font my-works__text">{t('worksFirstParagraph')}</p>
 
             <div className="my-works__container-works">
                 <Circles class="my-works__container-circle-1 circle" classImg="circle-1" src={Circle_01} />
@@ -34,9 +30,9 @@ const MyWorks = () => {
                         {/* If firstWork === true (the mouse is on the element) -> show the component, otherwise do not show */}
                         {firstWork ? <Work wrapper='show-wrapper'
                             link="https://map-d3js-lalita-klimchuk.netlify.app"
-                            firstText='Interactive world map' 
-                            secondText='Contains information about the location of containers and the status of documents for these containers' 
-                            thirdText='Skills: ReactJS/TypeScript/CSS/HTML/JSX' fourthText='Tools: d3-geo/geojson/Mui/Vite.js'/> : <></>}
+                            firstText={t('worksFirstHeader')} 
+                            secondText={t('worksFirstDescription')}
+                            thirdText={t('worksFirstSkills')} fourthText={t('worksFirstTools')}/> : <></>}
                         <img className="my-work__main-image" alt='Visualization of map project' id="img-1" src={Map} />
                     </div>
                     
@@ -48,9 +44,9 @@ const MyWorks = () => {
                         {/* If firstWork === true (the mouse is on the element) -> show the component, otherwise do not show */}
                         {secondWork ? <Work wrapper='show-wrapper' 
                             link="https://genshin-impact-lalita-klimchuk.netlify.app" 
-                            firstText='Forum for the game Genshin Impact' 
-                            secondText='These are ordinary static pages with text. The project was made to defend one of the subjects at the university' 
-                            thirdText='Skills: ReactJS/JS/CSS/HTML' fourthText='Tools: Google firebase/Styled Components'/> : <></>}
+                            firstText={t('worksSecondHeader')} 
+                            secondText={t('worksSecondDescription')} 
+                            thirdText={t('worksSecondSkills')} fourthText={t('worksSecondTools')}/> : <></>}
                         <img className="my-work__main-image" alt='Visualization of Genshin project' id="img-2" src={Genshin} />
                     </div>
 
@@ -60,7 +56,7 @@ const MyWorks = () => {
                 <div className="my-works__work work-1">
                     <div onMouseOver={() => setThirdWork(true)} onMouseLeave={() => setThirdWork(false)}>
                         {/* If firstWork === true (the mouse is on the element) -> show the component, otherwise do not show */}
-                        {thirdWork ? <Work wrapper='show-wrapper' firstText='Graduate work' secondText='Work is currently underway on this project. The project will be added to the portfolio when it is fully completed. But you can follow the development of this project on github :)' thirdText='Skills: ReactJS/TypeScript/CSS/HTML and etc' fourthText='Tools: Mui/Redux toolkit/Formik docs/Vite.js and etc'/> : <></>}
+                        {thirdWork ? <Work wrapper='show-wrapper' firstText={t('worksThirdHeader')} secondText={t('worksThirdDescription')} thirdText={t('worksThirdSkills')} fourthText={t('worksThirdTools')} /> : <></>}
                         <img className="my-work__main-image" alt='Visualization of Graduate project' id="img-3" src={UnderConstruction} />
                     </div>
 
