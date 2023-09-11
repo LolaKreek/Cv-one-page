21b5659b597b116a3b5468f3c704bc749fbbbb8f
Lalita Klimchuk <lalitaklimchuk@gmail.com>
2023-09-11 20:46:32 +0300

    Added translation into two languages of the contact me part

--- src/components/ContactMe/index.tsx
+++ src/components/ContactMe/index.tsx
@@ -7,9 +7,12 @@ import Circle_01 from "../../assets/circle_03.png";
 import { useFormik } from "formik";
 import { contactMeScheme } from "../../features/formik/formikScheme";
 import { allInputs, textAreas } from "../stylesVariable";
+import { useTranslation } from "react-i18next";
 
 //Creating a part "Contact Me"
 const ContactMe = () => {
+    const {t} = useTranslation();
+
     // Pass the useFormik() hook initial form values and a submit function that will
     // be called when the form is submitted
     const formik = useFormik({
@@ -37,18 +40,14 @@ const ContactMe = () => {
             <div>
                 <div className="contact-me__relative-wrapper">
                     <div className="contact-me__content-wrapper contact-wrapper">
-                        <h2 className="contact-wrapper__main-header main-header">//Contact me</h2>
-                        <h2 className="contact-wrapper__second-header second-header">Let's connect!</h2>
-                        <p className="standard-color-and-font contact-wrapper__paragraph">I am thrilled to be available for contact and excitedly looking forward 
-                        to connecting with you! Whether it's regarding career prospects, collaboration opportunities, or simply sharing 
-                        ideas, don't hesitate to reach out using the information provided below. As a dedicated and enthusiastic individual, 
-                        I'm eager to explore new possibilities and engage in meaningful conversations that can lead to mutual growth and success. 
-                        So, let's connect and embark on a journey of innovation and excellence together!</p>
+                        <h2 className="contact-wrapper__main-header main-header">{t('contactMe')}</h2>
+                        <h2 className="contact-wrapper__second-header second-header">{t('contactMeSecondHeader')}</h2>
+                        <p className="standard-color-and-font contact-wrapper__paragraph">{t('contactMeParagraph')}</p>
 
                         <form className='contact-wrapper__form' onSubmit={formik.handleSubmit}>
                             <TextField
                                 name="email"
-                                label="Your email"
+                                label={t('contactMeFormPlaceholderEmail')}
                                 variant="outlined"
                                 autoComplete="on"
                                 type="text"
@@ -61,7 +60,7 @@ const ContactMe = () => {
 
                             <TextField
                                 name="name"
-                                label="Subject"
+                                label={t('contactMeFromPlaceholderSubject')}
                                 variant="outlined"
                                 autoComplete="on"
                                 type="text"
@@ -74,7 +73,7 @@ const ContactMe = () => {
 
                             <TextField
                                 name="message"
-                                label="Message"
+                                label={t('contactMeFormPlaceholderMessage')}
                                 variant="outlined"
                                 autoComplete="on"
                                 type="textarea"
@@ -89,7 +88,7 @@ const ContactMe = () => {
 
                             <div className="form__submit-wrapper">
                                 <div className="submit__container" onClick={SubmitHandle}>
-                                    <p className="submit__submit-button">Send</p>
+                                    <p className="submit__submit-button">{t('contactMeFormSubmitButton')}</p>
                                     <Tooltip title="Sent the message">
                                         <SendIcon />
                                     </Tooltip>
@@ -113,12 +112,12 @@ const ContactMe = () => {
                         </div>
 
                         <div className="contact-image__text-container">
-                            <p className="standard-color-and-font">author: Lalita Klimchuk</p>
-                            <p className="standard-color-and-font">username: @LolaKreek</p>
-                            <p className="standard-color-and-font">description: Student</p>
-                            <p className="standard-color-and-font">occupation: Front-End Developer</p>
-                            <p className="standard-color-and-font">repository type: Open-source</p>
-                            <p className="standard-color-and-font">gitHub: https://github.com/LolaKreek</p>
+                            <p className="standard-color-and-font">{t('contactMeAuthor')}: Lalita Klimchuk</p>
+                            <p className="standard-color-and-font">{t('contactMeUsername')}: @LolaKreek</p>
+                            <p className="standard-color-and-font">{t('contactMedescription')}: Student</p>
+                            <p className="standard-color-and-font">{t('contactMeOccupation')}: Front-End Developer</p>
+                            <p className="standard-color-and-font">{t('contactMeRepositoryType')}: Open-source</p>
+                            <p className="standard-color-and-font">{t('contactMeGitHub')}: https://github.com/LolaKreek</p>
                         </div>
                     </div> 
 
