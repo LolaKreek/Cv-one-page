ed70b27dff946ded8c08ef3190a0bf13d85e23e3
Lalita Klimchuk <lalitaklimchuk@gmail.com>
2023-09-23 18:28:27 +0200

    Added text translation for Russian language

--- src/components/ChangeLanguage.tsx/index.tsx
+++ src/components/ChangeLanguage.tsx/index.tsx
@@ -64,6 +64,25 @@ const ChangeLanguage = () =>{
                         </g>
                     </svg>
                 </MenuItem>
+                <MenuItem value='ru'>
+                    <svg width="24" height="18" viewBox="0 0 24 18" fill="none" xmlns="http://www.w3.org/2000/svg">
+                        <g clipPath="url(#clip0_626_5281)">
+                            <g clipPath="url(#clip1_626_5281)">
+                                <path d="M-2.33301 0.900146V18.9001H27.667V0.900146H-2.33301Z" fill="#012169"/>
+                                <path d="M-2.33301 0.900146L27.667 18.9001L-2.33301 0.900146ZM27.667 0.900146L-2.33301 18.9001L27.667 0.900146Z" fill="black"/>
+                                <path d="M-2.33301 0.900146L27.667 18.9001M27.667 0.900146L-2.33301 18.9001" stroke="white" strokeWidth="3.63996"/>
+                                <mask id="mask0_626_5281" maskUnits="userSpaceOnUse" x="-3" y="0" width="31" height="19">
+                                    <path d="M12.667 9.90015H27.667V18.9001L12.667 9.90015ZM12.667 9.90015V18.9001H-2.33301L12.667 9.90015ZM12.667 9.90015H-2.33301V0.900146L12.667 9.90015ZM12.667 9.90015V0.900146H27.667L12.667 9.90015Z" fill="white"/>
+                                </mask>
+                                <g mask="url(#mask0_626_5281)">
+                                    <path d="M-2.33301 0.900146L27.667 18.9001L-2.33301 0.900146ZM27.667 0.900146L-2.33301 18.9001L27.667 0.900146Z" fill="black"/>
+                                    <path d="M-2.33301 0.900146L27.667 18.9001M27.667 0.900146L-2.33301 18.9001" stroke="#C8102E" strokeWidth="2.42664"/>
+                                </g>
+                                <path d="M-2.93359 7.50005H10.2664V0.300049H15.0664V7.50005H28.2664V12.3H15.0664V19.5H10.2664V12.3H-2.93359V7.50005Z" fill="#C8102E" stroke="white" strokeWidth="1.21332"/>
+                            </g>
+                        </g>
+                    </svg>
+                </MenuItem>
               </Select>
             </FormControl>
             )
