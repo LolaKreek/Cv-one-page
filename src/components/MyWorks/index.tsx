fd383bc9a2b47a4f67e8bdd3442dea8c132e1d65
Lalita Klimchuk <lalitaklimchuk@gmail.com>
2023-10-08 14:54:13 +0200

    Added work icons to the Map project

--- src/components/MyWorks/index.tsx
+++ src/components/MyWorks/index.tsx
@@ -36,8 +36,7 @@ const MyWorks = () => {
                         <img className="my-work__main-image" alt='Visualization of map project' id="img-1" src={Map} />
                     </div>
                     
-                    {/* <PortfolioIcons href="https://github.com/LolaKreek/Map-with-d3.js" showWebsite='https://map-d3js-lalita-klimchuk.netlify.app' /> */}
-                    <PortfolioIcons href="https://github.com/LolaKreek/Map-with-d3.js" />
+                    <PortfolioIcons href="https://github.com/LolaKreek/Map-with-d3.js" showWebsite='https://map-d3js-lalita-klimchuk.netlify.app' />
                 </div>
 
                 <div className="my-works__work work-1">
